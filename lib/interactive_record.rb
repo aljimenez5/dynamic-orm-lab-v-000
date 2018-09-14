ec7789cfe4d52b03f3beaa60ab502050eebb5f85
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-09-14 23:16:45 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -25,7 +25,7 @@ class InteractiveRecord
   end
   
   def col_names_for_insert
-    self.class.column_names.delete_if {|col_name| col_name == "id"} 
+    self.class.column_names.delete_if {|col_name| col_name == "id"}.join(', ')
   end
   
   def values_for_insert
