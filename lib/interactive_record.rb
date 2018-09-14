5db5e0305942f5c2d769ac7c6b45b59f04fe200f
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-09-14 23:55:48 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -50,6 +50,7 @@ class InteractiveRecord
     attribute.each do |key, value|
       att_key = key.to_s
       att_value = value.to_s 
+    end
     DB[:conn].execute("SELECT * FROM #{table_name} WHERE #{att_key} = ?", att_value)
   end
   
